e97637cab337d0f9cad35d512960a78e3d9e01d0
AriDevK <AriadneFave@hotmail.com>
2026-06-08 21:18:01 -0600

    feat(ui): changed background color based on scene toml values

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,26 +1,23 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
+import { useState, useEffect } from 'react';
+// import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import { Greet, GetBackground } from "../wailsjs/go/main/App";
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+    const [background, setBackground] = useState('transparent');
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+    useEffect(() => {
+        GetBackground().then(setBackground);
+    }, [])
 
     return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+        <div id="App"
+            style={{
+                backgroundColor: background,
+                width: "100%",
+                height: "100vh",
+            }}
+        >
         </div>
     )
 }
