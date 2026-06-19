11f24dfddef19c3c5875b4be8a3ff9f8af59ce6b
AriDevK <AriadneFave@hotmail.com>
2026-06-18 18:53:06 -0600

    checkpoint(save): checkpoint pre saved package deployed

---
+++ src/internal/crypt/crypt.go
@@ -0,0 +1,63 @@
+package crypt
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+)
+
+var secretKey = []byte("12345678901234567890123456789012") // 32 bytes = AES-256
+
+func Encrypt(data string) (string, error) {
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		return "", err
+	}
+
+	cipherData := gcm.Seal(nil, nonce, []byte(data), nil)
+	finalData := append(nonce, cipherData...)
+	encoded := base64.StdEncoding.EncodeToString(finalData)
+
+	return encoded, nil
+}
+
+func Decrypt(encodedData string) (string, error) {
+	data, err := base64.StdEncoding.DecodeString(encodedData)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return "", err
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(data) < nonceSize {
+		return "", err
+	}
+
+	nonce, cipherData := data[:nonceSize], data[nonceSize:]
+	plainData, err := gcm.Open(nil, nonce, cipherData, nil)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plainData), nil
+}
